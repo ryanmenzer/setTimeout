8489cc9d5b7194e3a62969fbe14c50b63161e193
ryanmenzer <ryan.menzer@cru.org>
2017-09-14 23:20:05 -0400

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,15 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
   title = 'Set Timeout';
-  @Input() time: number;
-  timer = 0;
+  time = 0;
 
-  startTimer(){
-    let s = 0;
-    setTimeout((_ => {
-      console.log(this.time);
-      while(s <= this.time){
-        this.timer = this.timer + 1;
-        s++;
-        console.log("s = " + s);
-      }
-    }),
-    1000);
+  startTimer(value){
+    while(this.time < value){
+      setTimeout(
+        this.time++,
+        1000
+      )
+    };
   };
 
 }
